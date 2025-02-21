26a124bc43cfe84b8d85aa1b6c8f6446c3c81864
Zhalgaspayeva <123243608+Zhalgaspayeva@users.noreply.github.com>
2025-02-21 23:12:07 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,2 +1,11 @@
 print("Zhalgaspayeva Ayakoz")
 print("Myrzakul Bekarys")
+
+import sys
+
+# Проверяем, переданы ли аргументы
+if len(sys.argv) > 1:
+    # Выводим все переданные имена
+    print("Names:", " ".join(sys.argv[1:]))
+else:
+    print("No names provided.")
